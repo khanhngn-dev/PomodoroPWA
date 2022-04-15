6fe237351cb8f84cbd49030ae7509db35f7a2173
KhanhGN <kgnguyen@myseneca.ca>
2022-04-15 13:16:30 +0700

    Uses spread operator to only change checked state

--- src/utils/reducer/list.utils/list.utils.ts
+++ src/utils/reducer/list.utils/list.utils.ts
@@ -9,7 +9,7 @@ export const checkItem = (
 	items.map((item, i) =>
 		i === index
 			? {
-					taskName: event.target.name,
+					...item,
 					complete: event.target.checked,
 					completedAt: event.target.checked ? createDate() : 'Not Complete',
 			  }
@@ -22,11 +22,30 @@ export const createDate = (): string => {
 	const minutes = date.getMinutes();
 	const day = date.getDate();
 	const month = date.getMonth();
-	const year = date.getFullYear();
-	return `${hours < 10 ? `0${hours}` : `${hours}`}:${
-		minutes < 10 ? `0${minutes}` : `${minutes}`
-	} - ${day < 10 ? `0${day}` : `${day}`}/${month < 10 ? `0${month}` : `${month}`}/${year}`;
+	const year = date.getFullYear() % 100;
+	return `${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`} ${
+		day < 10 ? `0${day}` : `${day}`
+	}/${month < 10 ? `0${month}` : `${month}`}/${year}`;
 };
 
 export const deleteItem = (items: ListItem[], index: number): ListItem[] =>
 	items.filter((item, i) => i !== index);
+
+export const sendNotification = (timerMode: boolean, time: number) => {
+	let min = Math.floor(time / 60);
+	let sec = time % 60;
+	let timeStr = `(for ${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec})`;
+	window?.Notification?.requestPermission()?.then((result) => {
+		if (result === 'granted') {
+			new Notification('From Pomodoro', {
+				body: `${
+					!timerMode ? `Let's take a break ${timeStr}` : `Let's get back to work ${timeStr}`
+				}`,
+			});
+		} else if (result === 'denied') {
+			alert(
+				`${!timerMode ? `Let's take a break ${timeStr}` : `Let's get back to work ${timeStr}`}`
+			);
+		}
+	});
+};
