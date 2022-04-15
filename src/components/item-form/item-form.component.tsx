9d7e1db628310d1e8de0b4a11c6d18cd4e6ce5c0
KhanhGN <kgnguyen@myseneca.ca>
2022-04-15 13:19:33 +0700

    Added support for description

--- src/components/item-form/item-form.component.tsx
+++ src/components/item-form/item-form.component.tsx
@@ -1,41 +1,65 @@
 import { FC, FormHTMLAttributes, FormEventHandler, useState, ChangeEventHandler } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItemToList } from '../../store/list/list.actions';
+import { selectTimerMode } from '../../store/timer/timer.selectors';
 
-import { FormContainer } from './item-form.styles';
 import ItemInput from '../item-input/item-input.component';
 import Button from '../button/button.component';
-import { selectTimerMode } from '../../store/timer/timer.selectors';
+import { FormContainer, InputContainer } from './item-form.styles';
+
+const defaultFormField: { taskName: string; description: string } = {
+	taskName: '',
+	description: '',
+};
 
 const ItemForm: FC<FormHTMLAttributes<HTMLFormElement>> = () => {
 	const dispatch = useDispatch();
-	const [taskName, setTaskName] = useState('');
+	const [formFields, setFormFields] = useState(defaultFormField);
 	const timerMode = useSelector(selectTimerMode);
+	const { taskName, description } = formFields;
 
-	const clearInput = () => setTaskName('');
+	const clearInput = () => setFormFields(defaultFormField);
 
 	const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
 		event.preventDefault();
-		if (!taskName) {
-			alert('Add a task name first');
-			return;
-		}
 		dispatch(
 			addItemToList({
 				taskName,
 				complete: false,
 				completedAt: 'Not Complete',
+				description,
 			})
 		);
 		clearInput();
 	};
 
 	const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-		setTaskName(event.target.value);
+		const { name, value } = event.target;
+		setFormFields({ ...formFields, [name]: value });
 	};
+
 	return (
 		<FormContainer onSubmit={handleSubmit}>
-			<ItemInput value={taskName} onChange={handleChange} placeholder='Kick Trong Nhan...' />
+			<InputContainer>
+				<ItemInput
+					required
+					name='taskName'
+					label={`Add a task name`}
+					value={taskName}
+					onChange={handleChange}
+					placeholder='Complete Pomodoro...'
+					maxLength={30}
+				/>
+				<ItemInput
+					required
+					name='description'
+					label={`Add some descriptions`}
+					value={description}
+					onChange={handleChange}
+					placeholder='Add more features...'
+					maxLength={100}
+				/>
+			</InputContainer>
 			<Button timerMode={timerMode} style={{ width: '500px' }}>
 				Add Task
 			</Button>
--- src/components/item-form/item-form.styles.tsx
+++ src/components/item-form/item-form.styles.tsx
@@ -9,3 +9,11 @@ export const FormContainer = styled.form`
 	border-radius: 20px;
 	width: 500px;
 `;
+
+export const InputContainer = styled.div`
+	width: 500px;
+	background-color: var(--color-grey);
+	padding: 20px;
+	border-radius: 20px;
+	margin-bottom: 20px;
+`;
