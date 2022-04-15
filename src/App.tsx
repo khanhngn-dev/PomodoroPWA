2e9987856f52b34f306d6885f7f4eb58f60113cf
KhanhGN <kgnguyen@myseneca.ca>
2022-04-15 13:18:17 +0700

    Changes name of component to more accurately reflect it

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { Routes, Route } from 'react-router';
 
 import Spinner from './components/spinner/spinner.component';
 const Navigation = lazy(() => import('./routes/navigation/navigation.component'));
-const Clock = lazy(() => import('./routes/home/home.component'));
+const Home = lazy(() => import('./routes/home/home.component'));
 
 function App() {
 	return (
 		<Suspense fallback={<Spinner />}>
 			<Routes>
 				<Route path='/' element={<Navigation />}>
-					<Route index element={<Clock />} />
+					<Route index element={<Home />} />
 				</Route>
 			</Routes>
 		</Suspense>
